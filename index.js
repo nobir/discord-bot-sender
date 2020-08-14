638a29c2a0cb711ae49c5b4520f8aed43e596979
Nobir <nobirfreelancer@gmail.com>
2020-08-14 15:52:54 +0600

    Added features Mention message

--- index.js
+++ index.js
@@ -8,16 +8,37 @@ client.once('ready', () => {
 	client.on('message', msg => {
 		if (!msg.guild || !msg.content.startsWith('!DM')) return
 
-		if (msg.guild && msg.content.startsWith(prefix)) {
-			let text = msg.content.slice(prefix.length)
+		let mentionUsers = msg.mentions.roles.map(item => item.members)
+		mentionUsers = mentionUsers[0].map(item => item.user) || [];
 
-			msg.guild.members.cache.forEach(member => {
-				if (member.id != client.user.id && !member.user.bot) {
-					member.send(text)
-					msg.channel.send("DM " + member.user.username)
-					// console.log("done!")
-				}
-			})
+		// console.log(mentionUsers)
+
+		if (msg.guild && msg.content.startsWith(prefix)) {
+			let text = msg.content.slice(prefix.length).replace(/\s<@&?[0-9>\s]+/gm, '')
+			let sendCount = 0
+			let botCount = 0
+			let notSendCout = 0
+
+			if (mentionUsers) {
+				mentionUsers.forEach((member) => {
+					if (member.bot) {
+						notSendCout++
+						botCount++
+						return
+					}
+
+					if (!member.bot) {
+						member.send(text)
+						msg.channel.send("DM " + member.username)
+						sendCount++;
+						// console.log("done!")
+					}
+				})
+			}
+
+			// console.log(text)
+
+			msg.reply('\n\n:rocket:  `Total Send: ' + sendCount + '`\n\n:x:  `Total Not Send: ' + notSendCout + '`\n\n:frog:  `Total Bot: ' + botCount + '`')
 		}
 	})
 })
